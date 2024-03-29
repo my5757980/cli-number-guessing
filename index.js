5d7f14014c43d13c37ab23a03febcfaa4c674fc5
Maira Naeem <163671644+mairanoor412@users.noreply.github.com>
2024-03-29 04:43:54 -0700

    created a cli number guessing

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1)computer will generate a randomNumber
+// 2)user input for guessing number
+// 3)compare user input with computer generate number and show result
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message: "please guess a number between 1-6",
+    }
+]);
+if (answer.userGussedNumber === randomNumber) {
+    console.log("congratulation you guess right number");
+}
+else {
+    console.log("you guess wrong number");
+}
